0159cd7384a02633f1091750f9da619b55e8b168
AkeKung <kingake555@gmail.com>
2019-03-23 02:27:35 +0700

    Database #1.2

--- Database/login.hpp
+++ Database/login.hpp
@@ -6,29 +6,21 @@ using namespace std;
 
 class Login{
 	public :
-		bool login(void);
+		bool login(string ,string);
 };
 
-bool Login::login(){
+bool Login::login(string input_id,string input_pass){
 	ifstream ID("Text/ID.txt");
     ifstream Pass("Text/Pass.txt");
     string line_id,line_pass;
-    string input_id,input_pass;
     bool check=false;
     int num_check=0;
-    do{
-    cout<<"Plese Input ID: ";
-    getline(cin,input_id);
-    cout<<"Plese Input PASS: ";
-    getline(cin,input_pass);
     while(getline(ID,line_id)){
     	getline(Pass,line_pass);
     	if(line_id==input_id && line_pass==input_pass){
     		check=true;
     		break;
-		}
-	}
-	num_check++;
+        }
 }while(check==false &&num_check<=2);
 	return check;
 };
