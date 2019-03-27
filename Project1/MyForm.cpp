0a2c30cbc2120515a7b0fbe9d5832944c1689e9a
AkeKung <kingake555@gmail.com>
2019-03-27 21:34:06 +0700

    Add project files.

---
+++ Project1/MyForm.cpp
@@ -0,0 +1,19 @@
+﻿#include "MyForm.h"
+#include <stdio.h>  // for printf
+#include "Server_select.h"
+//#include <SQLAPI.h> // main SQLAPI++ header
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+int main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Project1::MyForm form;
+	Project1::Server_select Server_select;
+	Application::Run(%form);
+	return 0;
+}
