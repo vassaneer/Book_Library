801872b9e52e69ca824da6ba8f7677ee01f153b2
AkeKung <kingake555@gmail.com>
2019-04-02 23:15:12 +0700

    move new file

---
+++ GUIBOOK/MyForm.cpp
@@ -0,0 +1,22 @@
+#include "MyForm.h"
+
+#include <stdio.h>		// for printf
+#include "Server_select.h"		//for other window
+#include "Register_ID.h"		//for other window
+#include "Ruleofuse.h"			//for other window
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+int main()//array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	GUIBOOK::MyForm form;
+	Application::Run(%form);
+	return 0;
+}
+
---
+++ GUIBOOK/MyForm.h
@@ -0,0 +1,346 @@
+#pragma once
+#include <stdio.h>		// for printf
+#include "Server_select.h"		//for other window
+#include "Register_ID.h"		//for other window
+#include "Ruleofuse.h"			//for other window
+#include "data_include.h"
+#include <msclr\marshal_cppstd.h>
+
+namespace GUIBOOK {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::TextBox^  IDbox;
+	private: System::Windows::Forms::TextBox^  Passwordbox;
+
+
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::GroupBox^  groupBox1;
+
+	private: System::Windows::Forms::PictureBox^  pictureBox2;
+	private: System::Windows::Forms::Label^  label4;
+	private: System::Windows::Forms::MonthCalendar^  monthCalendar1;
+	private: System::Windows::Forms::Button^  button3;
+
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->IDbox = (gcnew System::Windows::Forms::TextBox());
+			this->Passwordbox = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->monthCalendar1 = (gcnew System::Windows::Forms::MonthCalendar());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->groupBox1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 36, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(435, 156);
+			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(393, 69);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Arn-Sue-Gun";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(25, 43);
+			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(148, 29);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Student ID :";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(44, 85);
+			this->label3->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(142, 29);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"Password :";
+			// 
+			// IDbox
+			// 
+			this->IDbox->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->IDbox->Location = System::Drawing::Point(200, 44);
+			this->IDbox->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->IDbox->MaxLength = 9;
+			this->IDbox->Name = L"IDbox";
+			this->IDbox->Size = System::Drawing::Size(132, 26);
+			this->IDbox->TabIndex = 3;
+			this->IDbox->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			this->IDbox->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// Passwordbox
+			// 
+			this->Passwordbox->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Passwordbox->Location = System::Drawing::Point(200, 86);
+			this->Passwordbox->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->Passwordbox->MaxLength = 20;
+			this->Passwordbox->Name = L"Passwordbox";
+			this->Passwordbox->Size = System::Drawing::Size(132, 26);
+			this->Passwordbox->TabIndex = 4;
+			this->Passwordbox->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			this->Passwordbox->UseSystemPasswordChar = true;
+			// 
+			// button1
+			// 
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::System;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(73, 143);
+			this->button1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(119, 41);
+			this->button1->TabIndex = 5;
+			this->button1->Text = L"Register";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::System;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button2->Location = System::Drawing::Point(215, 143);
+			this->button2->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(119, 41);
+			this->button2->TabIndex = 6;
+			this->button2->Text = L"Login";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->IDbox);
+			this->groupBox1->Controls->Add(this->Passwordbox);
+			this->groupBox1->Controls->Add(this->button1);
+			this->groupBox1->Controls->Add(this->label2);
+			this->groupBox1->Controls->Add(this->button2);
+			this->groupBox1->Controls->Add(this->label3);
+			this->groupBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->groupBox1->Location = System::Drawing::Point(243, 274);
+			this->groupBox1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Padding = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->groupBox1->Size = System::Drawing::Size(379, 209);
+			this->groupBox1->TabIndex = 7;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"Sigh in";
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
+			this->pictureBox2->Location = System::Drawing::Point(609, 1);
+			this->pictureBox2->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(212, 111);
+			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox2->TabIndex = 9;
+			this->pictureBox2->TabStop = false;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(222)));
+			this->label4->Location = System::Drawing::Point(571, 725);
+			this->label4->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(277, 31);
+			this->label4->TabIndex = 10;
+			this->label4->Text = L"Arn-Sue-Gun V.beta";
+			// 
+			// monthCalendar1
+			// 
+			this->monthCalendar1->Location = System::Drawing::Point(24, 516);
+			this->monthCalendar1->Margin = System::Windows::Forms::Padding(12, 11, 12, 11);
+			this->monthCalendar1->Name = L"monthCalendar1";
+			this->monthCalendar1->TabIndex = 11;
+			this->monthCalendar1->DateChanged += gcnew System::Windows::Forms::DateRangeEventHandler(this, &MyForm::monthCalendar1_DateChanged);
+			// 
+			// button3
+			// 
+			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(222)));
+			this->button3->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button3.Image")));
+			this->button3->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button3->Location = System::Drawing::Point(383, 516);
+			this->button3->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(116, 38);
+			this->button3->TabIndex = 12;
+			this->button3->Text = L"Exit";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(820, 754);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->monthCalendar1);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->pictureBox2);
+			this->Controls->Add(this->groupBox1);
+			this->Controls->Add(this->label1);
+			this->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->MaximizeBox = false;
+			this->MaximumSize = System::Drawing::Size(838, 801);
+			this->MinimumSize = System::Drawing::Size(838, 801);
+			this->Name = L"MyForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"[ASN]ArnSueGun";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+
+	}
+int i = 0;
+private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+	DatabaseApi s;
+	String ^ id = IDbox->Text;
+	String ^ pass = Passwordbox->Text;
+	string id_convert = msclr::interop::marshal_as<string>(id);
+	string pass_convert = msclr::interop::marshal_as<string>(pass);
+	if (i == 2) {
+		MessageBox::Show("Eror");
+		//this->Hide();
+		//block_time block;
+		//block.ShowDialog();
+		//this->Show();
+		i = 0;
+	}
+	else if (s.check_login(id_convert, pass_convert) == 1) {
+		MessageBox::Show("Welcome!!!");
+		this->Hide();
+		Server_select server;
+		server.ShowDialog();
+	}
+	else if (s.check_login(id_convert, pass_convert) == 2) {
+		i++;
+		MessageBox::Show("password incorrect!!!");
+		Passwordbox->Text = "";
+	}
+	else if (id_convert == "" || pass_convert == "") {
+		MessageBox::Show("Please add data!!!");
+		IDbox->Text = "";
+		Passwordbox->Text = "";
+		i++;
+	}
+	else {
+		i++;
+		MessageBox::Show("No User!!!");
+		IDbox->Text = "";
+		Passwordbox->Text = "";
+	}
+
+}
+private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {	
+	
+}
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+	this->Hide();
+	//Register_ID^ form2 = gcnew Register_ID();
+	Register_ID regist_page;
+	MessageBox::Show("Please fill out the form.");
+	regist_page.ShowDialog();
+	this->Show();
+
+
+	//this->Hide();		//after press botton. hide this window.
+
+	//MessageBox::Show("Please fill out the form.");
+
+	//Register_ID regis;		//other window
+	//regis.ShowDialog();		//show other window
+
+
+}
+private: System::Void monthCalendar1_DateChanged(System::Object^  sender, System::Windows::Forms::DateRangeEventArgs^  e) {
+}
+private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
+	Application::Exit();
+}
+};
+}
